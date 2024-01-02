affdd25647e1fb3d6d84148f6834c7ee83245f5c
Deodato <39544268+deodatocunha@users.noreply.github.com>
2024-01-02 19:52:01 +0000

    adiciona arquivos `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1> Ana Laura, Deodato, Pedro e Sabrina!!!! Que familiaa </h1>
+  return <h1> Ana Laura, Deodato, Pedro e Sabrina!!!! Que familiaa </h1>;
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
